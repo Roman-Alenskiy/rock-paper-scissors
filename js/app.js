a5e10678db6c6f9c0fd542a12327e8647032021c
Roman Alenskiy <kuperman88@gmail.com>
2018-09-21 21:15:09 +0300

    Finished front-end

--- js/app.js
+++ js/app.js
@@ -1,73 +1,118 @@
-let shapes = ["Rock", "Paper", "Scissors"]
-
-function capitalize(string) {
-    return string.slice(0, 1).toLocaleUpperCase() + string.slice(1).toLocaleLowerCase();
-}
-
 function computerPlay() {
+    let shapes = ["rock", "paper", "scissors"]
     return shapes[Math.floor(Math.random() * shapes.length)]
 }
 
-function playerPlay() {
-    let input = prompt("Choose Rock, Paper or Scissors!");
-    return inputCheck(input)
+function playRound() {
+    let computerSelect = computerPlay();
+    console.log(computerSelect)
+    let playerSelect = this.id;
+    console.log(playerSelect)
+    let roundResult = defineRoundResult(computerSelect, playerSelect);
+    setAnnouncerText(roundResult)
+    editAnnouncerStyle(roundResult);
+    editScore(roundResult);
+    gameEndCheck();
 }
 
-function inputCheck(input) {
-    input = capitalize(input);
-    if (!shapes.includes(input)) {
-        alert("Incorrect input!");
-        return playerPlay();
-    } else {
-        return input;
-    }
-}
-
-function playRound(computerSelect, playerSelect) {
+function defineRoundResult(computerSelect, playerSelect) {
     if (computerSelect === playerSelect) {
         return "Draw!"
-    }   else if (computerSelect === "Rock" && playerSelect === "Paper") {
+    }   else if (computerSelect === "rock" && playerSelect === "paper") {
         return "You win!"
-    }   else if (computerSelect === "Rock" && playerSelect === "Scissors") {
+    }   else if (computerSelect === "rock" && playerSelect === "scissors") {
         return "You lose! Rock beats Scissors!"
-    }   else if (computerSelect === "Paper" && playerSelect === "Scissors") {
+    }   else if (computerSelect === "paper" && playerSelect === "scissors") {
         return "You win!"
-    }   else if (computerSelect === "Paper" && playerSelect === "Rock") {
+    }   else if (computerSelect === "paper" && playerSelect === "rock") {
         return "You lose! Paper beats Rock!"
-    }   else if (computerSelect === "Scissors" && playerSelect === "Rock") {
+    }   else if (computerSelect === "scissors" && playerSelect === "rock") {
         return "You win!"
-    }   else if (computerSelect === "Scissors" && playerSelect === "Paper") {
+    }   else if (computerSelect === "scissors" && playerSelect === "paper") {
         return "You lose! Scissors beats Paper!"
     }
 }
 
-function winnerCheck(playerWins, computerWins) {
-    if (playerWins === computerWins) {
-        return "Game is over: Draw!"
-    } else if (playerWins > computerWins) {
-        return "Game is over: You win!"
-    } else {
-        return "Game is over: You lose!"
+function gameEndCheck() {
+    rounds--
+    if (rounds <= 0) {
+        let gameResult
+        if (playerWins === computerWins) {
+            setAnnouncerText(gameResult = "Game is over: Draw!")
+        } else if (playerWins > computerWins) {
+            setAnnouncerText(gameResult = "Game is over: You win!")
+        } else {
+            setAnnouncerText(gameResult = "Game is over: You lose!")
+        };
+        editAnnouncerStyle(gameResult)
+        announcer.style.fontWeight = "bold"
+        setStartButton("Start new game!")
+        chooseButtons.forEach(function(button) {
+            button.removeEventListener('click', playRound)
+        })
     }
+}
+
+function setStartButton(message) {
+    startButton.textContent = message
+}
+
+function editAnnouncerStyle(result) {
+    announcer.style.fontWeight = "normal"
+    if (result === "You win!" || result === "Game is over: You win!") {
+        announcer.style.backgroundColor = '#d8ffd3'
+    } else if (result === "Draw!" || result === "Game is over: Draw!") {
+        announcer.style.backgroundColor = '#d3eaff'
+    } else {
+        announcer.style.backgroundColor = '#ffd3d3'
+    }
+}
+
+function resetAnnouncerStyle() {
+    announcer.style.backgroundColor = '#d3eaff'
+    announcer.style.fontWeight = "normal"
+}
+
+function setAnnouncerText(message) {
+    announcer.textContent = message
+}
+
+function editScore(roundResult) {
+    if (roundResult === "You win!") {
+        playerWins++
+        playerScore.textContent = playerWins;
+    } else if (roundResult !== "Draw!") {
+        computerWins++
+        computerScore.textContent = computerWins
+    }
+}
+
+function resetScore() {
+    playerWins = computerWins = playerScore.textContent = computerScore.textContent = 0;
+    
 }
 
 function game() {
-    let rounds = prompt("How much rounds are you want to play?");
-    let playerWins = computerWins = 0;
+    rounds = prompt("How much rounds are you want to play?");
 
-    for (let i = 0; i < rounds; i++) {
-        let roundResult = playRound(computerPlay(), playerPlay());
+    setStartButton("Restart!")
+    resetAnnouncerStyle();
+    setAnnouncerText("Make your choice!");
+    resetScore();
 
-        if (roundResult === "You win!") {
-            playerWins++
-        } else if (roundResult !== "Draw!") {
-            computerWins++
-        };
-
-        alert(`${roundResult}\nYou - ${playerWins}, Computer - ${computerWins}`)
-    }
-
-    alert(`${winnerCheck(playerWins, computerWins)}\n${playerWins}:${computerWins}`);
+    chooseButtons.forEach(function(button) {
+        button.addEventListener('click', playRound)
+    })
 }
 
-game();
+const startButton = document.querySelector('#start-game')
+const chooseButtons = document.querySelectorAll('.btn-choose')
+const announcer = document.querySelector('#announcer')
+const playerScore = document.querySelector('#player-score')
+const computerScore = document.querySelector('#computer-score')
+
+let playerWins
+let computerWins
+let rounds
+
+startButton.addEventListener('click', game)
